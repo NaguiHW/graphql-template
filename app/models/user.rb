6fc0d4f504ae06fb20cf8a67e38d37deebb35335
Kalib Hackin <nagui08@hotmail.com>
2023-09-09 11:15:42 -0500

    updated User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,12 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validate :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
-  validate :password, presence: true, length: { minimum: 6 }
-  validate :nickname, presence: true
+  # minimum of 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character
+  PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[[:^alnum:]])/x
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            format: { with: PASSWORD_REGEX }
+  validates :nickname, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
 end
