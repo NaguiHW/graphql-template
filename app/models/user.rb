b2fba5d5a94e20734b1e55b292685b94e6595b68
Kalib Hackin <nagui08@hotmail.com>
2023-09-09 10:56:40 -0500

    adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validate :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
+  validate :password, presence: true, length: { minimum: 6 }
+  validate :nickname, presence: true
 end
